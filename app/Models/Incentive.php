6e698f1129de255fae16c7722572a90a3b86e50b
faizal.techzar <faizal.techzar@gmail.com>
2024-12-19 16:29:31 +0530

    Update Incentive.php

--- app/Models/Incentive.php
+++ app/Models/Incentive.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\DataCenterNew;
+use App\Models\datacenternew;
 use App\Models\Que;
 
 class Incentive extends Model
@@ -13,7 +13,7 @@ class Incentive extends Model
 
     public function datacenternews()
 {
-    return $this->belongsTo(DataCenterNew::class, 'datacenter_id');
+    return $this->belongsTo(datacenternew::class, 'datacenter_id');
 }
 
 public function ques()
