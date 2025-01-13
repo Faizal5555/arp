d03d73a045f19b766b23654686d5239e36524979
faizal.techzar <faizal.techzar@gmail.com>
2025-01-13 15:58:45 +0530

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1102,6 +1102,7 @@ table tfoot th {
                 <ul class="nav flex-column sub-menu1">
                   <li class="nav-item"> <a class="nav-link" style="font-size:0.83rem   !important;margin-left:-4px !important;" href="{{route('Supplier.create')}}">Add Supplier</a></li>
                   <li class="nav-item"> <a class="nav-link" style="font-size:0.83rem   !important;margin-left:-4px !important;"  href="{{route('SuperLiner.cost')}}">Cost Request (RFQ) </a></li>
+                  <li class="nav-item"> <a class="nav-link" style="font-size: 0.83rem  !important;margin-left:-4px !important;"  href="{{route('supplier.costRequestView')}}">Total Cost Requests (RFQ) </a></li>
                   <!--<li class="nav-item"> <a class="nav-link"  href="{{route('supplier.view')}}">View Supplier</a></li>-->
                 </ul>
               </div>
