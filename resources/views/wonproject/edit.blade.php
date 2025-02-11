c08f3e0a23604849d149bac9a4319c6e454428fd
faizal.techzar <faizal.techzar@gmail.com>
2025-02-11 12:34:59 +0530

    update edit wonproject

--- resources/views/wonproject/edit.blade.php
+++ resources/views/wonproject/edit.blade.php
@@ -739,12 +739,12 @@ input.txtCal.valid {
                                                             </label>
                                                         </td>
                                                         <td></td>
-                                                        <td>
+                                                        {{-- <td>
                                                             <button class="float-left ml-2 btn btn-success addvendor"
                                                                 data-vendor="0" data-count="0" type="button">
                                                                 Add table
                                                             </button>
-                                                        </td>
+                                                        </td> --}}
                                                     </tr>
 
                                                     <!-- Client/Vendor Name -->
@@ -1382,7 +1382,8 @@ input.txtCal.valid {
                                                                             </tr>
 
                                                                         </tbody>
-                                                                    </table><button
+                                                                    </table>
+                                                                    {{-- <button
                                                                         class="p-2 ml-2 btn btn-danger removetable"
                                                                         type="button">
                                                                         <svg class="svg-inline--fa fa-xmark"
@@ -1395,7 +1396,7 @@ input.txtCal.valid {
                                                                                 d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z">
                                                                             </path>
                                                                         </svg><!-- <i class="fa-solid fa-xmark"></i> Font Awesome fontawesome.com -->
-                                                                    </button>
+                                                                    </button> --}}
                                                                 </div>
                                                                 @endfor
                                                                 @endif
@@ -1431,15 +1432,15 @@ input.txtCal.valid {
                                                                         </select>
                                                                         </label>
                                                                     </td>
-                                                                    <td>
+                                                                    {{-- <td>
                                                                         <button class="ml-2 btn btn-success addvendor"
                                                                             data-vendor="1" data-count="{{ $key }}"
                                                                             type="button">
                                                                             Add table
                                                                         </button>
-                                                                    </td>
+                                                                    </td> --}}
                                                                     <td>
-                                                                        <button
+                                                                        {{-- <button
                                                                             class="ml-2 btn btn-danger removecountry"
                                                                             type="button">
                                                                             <svg class="svg-inline--fa fa-xmark"
@@ -1453,7 +1454,7 @@ input.txtCal.valid {
                                                                                     d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z">
                                                                                 </path>
                                                                             </svg><!-- <i class="fa-solid fa-xmark"></i> Font Awesome fontawesome.com -->
-                                                                        </button>
+                                                                        </button> --}}
                                                                     </td>
                                                                 </tr>
                                                                 <tr>
@@ -2059,7 +2060,8 @@ input.txtCal.valid {
                                                                                             </td>
                                                                                         </tr>
                                                                                     </tbody>
-                                                                                </table><button
+                                                                                </table>
+                                                                                {{-- <button
                                                                                     class="p-2 ml-2 btn btn-danger removetable"
                                                                                     type="button">
                                                                                     <svg class="svg-inline--fa fa-xmark"
@@ -2075,7 +2077,7 @@ input.txtCal.valid {
                                                                                             d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z">
                                                                                         </path>
                                                                                     </svg><!-- <i class="fa-solid fa-xmark"></i> Font Awesome fontawesome.com -->
-                                                                                </button>
+                                                                                </button> --}}
                                                                             </div>
                                                                             @endfor
                                                                             @endif
